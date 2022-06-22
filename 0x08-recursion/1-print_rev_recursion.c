00b4e2580792297cc466522bb589a3c1a68ed755
Kirubel Moges <kmoges758@gmail.com>
2022-06-22 09:26:19 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _print_rev_recursion - Prints a string in reverse
  *
- * Return: Always 0.
+ * @s: Pointer to the string
+ *
+ * Return: void
+ *
  */
-int main(void)
+
+void _print_rev_recursion(char *s)
 {
-    _print_rev_recursion("\nColton Walker");
-    return (0);
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
